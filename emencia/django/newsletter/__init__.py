d4329870f0a4b2a1492f3d199d2c8205b92fa1a5
Maciej Janiszewski <chleb@krojony.pl>
2016-10-03 23:55:10 +0200

    readd metadata

---
+++ emencia/django/newsletter/__init__.py
@@ -0,0 +1,8 @@
+"""emencia.django.newsletter"""
+__version__ = '0.3'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42, pisarz1958'
+__email__ = 'fantomas42@gmail.com, pisarzk@gmail.com'
+
+__url__ = 'http://emencia.fr/'
